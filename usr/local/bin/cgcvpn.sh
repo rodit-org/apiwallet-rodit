c3e2ba2ea4693728839c30491f27f2fa8256246e
cableguard@cableguard.org <cableguard@cableguard.org>
2024-06-07 13:29:47 +0200

    improved cgvpn

--- usr/local/bin/cgcvpn.sh
+++ usr/local/bin/cgcvpn.sh
@@ -4,11 +4,33 @@
 # Copyright (C) 2023 Vicente Aceituno Canal vpn@cableguard.org All Rights Reserved.
 
 # minor version is odd for testnet, even for mainnet
-VERSION="1.3.31"
+VERSION="1.3.35"
+
+# Function to restore network settings
+shutdown_vpn() {
+    if [ -f ~/network_settings_backup ]; then
+        echo "Restoring original network settings..."
+        interface_name=$(sudo wg show | awk '/^interface:/ {print $2}')
+        server_ip=$(sudo wg show $interface_name | grep "endpoint:" | awk '{print $2}' | cut -d ':' -f 1)
+        default_gateway=$(ip route | awk '/default/ {print $3}')
+        physical_device_name=$(ip -4 route show default | awk '/default via/ {print $5}')
+        sudo ip route delete 128.0.0.0/1 dev "$interface_name"
+        sudo ip route delete 0.0.0.0/1 dev "$interface_name"
+        sudo ip route delete $server_ip via $default_gateway dev $physical_device_name
+        echo "Network settings restored."
+    else
+        echo "No backup found. Cannot restore network settings."
+    fi
+}
+
+# Check for shutdown option
+if [ "$1" == "shutdown" ]; then
+    shutdown_vpn
+    exit 0
+fi
 
 # Print script information
-# export NFTCONTRACTID=$(cat ~/cgtun/cgsh/account)
-echo "Version" $VERSION "running on " $BLOCKCHAIN_ENV "at Smart Contract" $NFTCONTRACTID " Get help with: "$0" help"
+echo "Version" $VERSION "running on " $BLOCKCHAIN_ENV "at Smart Contract" $RODITCONTRACTID " Get help with: "$0" help"
 
 # Check if there are no entry parameters
 if [ $# -eq 0 ]; then
@@ -18,10 +40,11 @@ fi
 
 if [ "$1" == "help" ]; then
     echo "Usage: "$0" [account_id] [Options]"
-    echo "Works best when called from the cgtun directory"
+    echo "Warning: This script is not designed to work with multihomed computers"
     echo ""
     echo "Options:"
     echo "  "$0" <json_file_name> (without extension)"
+    echo "  "$0" shutdown - Shutdown VPN and original network settings"
     exit 0
 fi
 
@@ -32,9 +55,18 @@ if [ ! -f "$json_file" ]; then
     exit 1
 fi
 
+# Get the file permissions in numeric form
+PERMISSIONS=$(stat -c "%a" "$json_file")
+# Check if the permissions are not 600
+if [ "$PERMISSIONS" -ne 600 ]; then
+    echo "IMPORTANT Warning: The file '$json_file' has permissions $PERMISSIONS, which is not 600 (secure)."
+else
+    echo "The file '$json_file' has secure permissions."
+fi
+
 # Run cableguard and start the tunnel
-echo "sudo cableguard-cli -v trace $json_file >> ~/cableguard.$1.log 2>&1"
-if sudo cableguard-cli -v trace $json_file >> ~/cableguard.$1.log 2>&1; then
+echo "sudo cableguard-cli -v trace $json_file"
+if sudo cableguard-cli -v trace $json_file; then
     echo "cableguard-cli: Started and created the tunnel."
 else
     echo "Error: cableguard-cli failed to start."
@@ -65,7 +97,7 @@ sudo nmcli connection modify $interface_name ipv4.dns "$dns_resolver"
 if [ -n "$interface_name" ]; then
     # Update bring the interface up
     echo "sudo ip link set "$interface_name" up"
-    if sudo ip link set "$interface_name" up >> ~/cableguard.$1.log 2>&1; then
+    if sudo ip link set "$interface_name" up; then
         echo "Bringing up interface: '$interface_name'."
     else
         echo "Error: Could not bring interface up"
@@ -73,24 +105,22 @@ if [ -n "$interface_name" ]; then
     fi
 
     # Fetching the default gateway IP address
-    DEFAULT_GATEWAY=$(ip route | awk '/default/ {print $3}')
+    default_gateway=$(ip route | awk '/default/ {print $3}')
 
     # Fetching the physical device IP address
-    PHYSICAL_DEVICE_NAME=$(ip -4 route show default | awk '/default via/ {print $5}')
+    physical_device_name=$(ip -4 route show default | awk '/default via/ {print $5}')
 
     # Fetching the server IP address, This is read from the A entry of the vpn server of the RODiT
     # or read again when running wg subdomain-peer to change server
-    SERVER_IP=$(sudo wg show $interface_name | grep "endpoint:" | awk '{print $2}' | cut -d ':' -f 1)
+    server_ip=$(sudo wg show $interface_name | grep "endpoint:" | awk '{print $2}' | cut -d ':' -f 1)
 
     # Adding route using obtained values
-    echo "sudo ip route add $SERVER_IP via $DEFAULT_GATEWAY dev $PHYSICAL_DEVICE_NAME"
-    sudo ip route add $SERVER_IP via $DEFAULT_GATEWAY dev $PHYSICAL_DEVICE_NAME
-    # sudo ip route del default via 0.0.0.0 dev wlo1
-    # sudo ip route add 134.209.232.255 via 192.168.18.1 dev wlo1
+    echo "sudo ip route add $server_ip via $default_gateway dev $physical_device_name"
+    sudo ip route add $server_ip via $default_gateway dev $physical_device_name
 
     # Update iptables rules
     echo "sudo ip route add 0.0.0.0/1 dev "$interface_name""
-    if sudo ip route add 0.0.0.0/1 dev "$interface_name" >> ~/cableguard.$1.log 2>&1; then
+    if sudo ip route add 0.0.0.0/1 dev "$interface_name"; then
         echo "Default Gateway 0.0.0.0/1 rule: Added for interface '$interface_name'."
     else
         echo "Error: Failed to add iptables routing rule."
@@ -98,7 +128,7 @@ if [ -n "$interface_name" ]; then
     fi
 
     echo "sudo ip route add 128.0.0.0/1 dev "$interface_name""
-    if sudo ip route add 128.0.0.0/1 dev "$interface_name" >> ~/cableguard.$1.log 2>&1; then
+    if sudo ip route add 128.0.0.0/1 dev "$interface_name"; then
         echo "Default Gateway 128.0.0.0/1 rule: Added for interface '$interface_name'."
     else
         echo "Error: Failed to add iptables routing rule."
